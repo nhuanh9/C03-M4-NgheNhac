4f82f9d215bc714621e1396ea6fb468896230abd
Hiep <tranvanhiep992001@gmail.com>
2023-08-04 14:41:15 +0700

    tim kiem theo id playlist

--- src/controller/playlistController.ts
+++ src/controller/playlistController.ts
@@ -2,21 +2,23 @@ import playlistService from "../service/playlistService";
 
 class PlaylistController{
     findAll = async (req, res) => {
-        let {userId, asc, desc} = req.query
-        if (userId == undefined && asc == undefined && desc == undefined){
+        let {userId, asc, desc, id} = req.query
+        if (userId == undefined && asc == undefined && desc == undefined && id == undefined){
             let list = await playlistService.findAll();
             res.json(list)
-        } else if (userId != undefined && asc == undefined && desc == undefined){
+        } else if (userId != undefined && asc == undefined && desc == undefined && id == undefined){
             let list = await playlistService.findByUserName(Number(userId));
             res.json(list)
-        } else if (userId == undefined && asc == '' && desc == undefined){
+        } else if (userId == undefined && asc == '' && desc == undefined && id == undefined){
             let list = await playlistService.getSortByAsc()
             res.json(list)
-        } else if (userId == undefined && asc == undefined && desc == ''){
+        } else if (userId == undefined && asc == undefined && desc == '' && id == undefined){
             let list = await playlistService.getSortByDesc()
             res.json(list)
+        } else if (userId == undefined && asc == undefined && desc == undefined && id != undefined){
+            let list = await playlistService.findById(id)
+            res.json(list)
         }
-
     }
     add = async (req, res) => {
         let data = await playlistService.add(req.body);
